7d094af9caf23f95d386e6383eff851f277d5e00
Arkar Myat <arkardev00@gmail.com>
2024-10-05 12:34:01 -0400

    mod test and pop

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -30,4 +30,39 @@ impl List {
         };
         self.head = Link::More(Box::new(new_node));
     }
+
+    fn pop(&mut self) -> Option<i32> {
+        let result;
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => {
+                result = None;
+            }
+            Link::More(node) => {
+                result = Some(node.elem);
+                self.head = node.next;
+            }
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        assert_eq!(list.pop(), Some(4));
+
+    }
 }
